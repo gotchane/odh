6c2058ce19f2ffdee98b1b825118e3b4a4fa1a13
Masayuki Goto <originals0711@gmail.com>
2020-01-31 09:39:56 +0900

    feat: loading session from aws profile

--- main.go
+++ main.go
@@ -1,19 +1,62 @@
 package main
 
-import(
+import (
 	"fmt"
 	"flag"
+    "github.com/c-bata/go-prompt"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/opsworks"
 )
 
+var (
+	suggests []prompt.Suggest
+)
+
+func fetchSuggestStacks(profile, region string) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{Profile:profile}))
+	svc := opsworks.New(sess, aws.NewConfig().WithRegion(region))
+	result, err := svc.DescribeStacks(nil)
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range result.Stacks {
+		suggests = append(suggests, prompt.Suggest{Text: aws.StringValue(b.Name)})
+	}
+}
+
+func completer(in prompt.Document) []prompt.Suggest {
+    return prompt.FilterHasPrefix(suggests, in.GetWordBeforeCursor(), true)
+}
+
 func main() {
-	var profile string
+	var (
+		profile string
+		region string
+	)
+
 	flag.StringVar(&profile, "p", "", "Aws profile")
+	flag.StringVar(&region, "r", "ap-northeast-1", "Aws region")
 	flag.Parse()
 
-	if (profile != "staging") {
+	if (profile == "") {
 		fmt.Println("Not existing profile:", profile)
 		flag.Usage()
 		return
 	}
-	fmt.Println(profile)
+
+	fetchSuggestStacks(profile, region)
+
+    in := prompt.Input(
+		">>> ",
+		completer,
+		prompt.OptionTitle("opsworks-helper"),
+		prompt.OptionHistory(nil),
+		prompt.OptionPrefixTextColor(prompt.Yellow),
+		prompt.OptionPreviewSuggestionTextColor(prompt.Blue),
+		prompt.OptionSelectedSuggestionBGColor(prompt.LightGray),
+		prompt.OptionSuggestionTextColor(prompt.Black),
+		prompt.OptionSuggestionBGColor(prompt.Blue),
+	)
+    fmt.Println("Your input: " + in)
 }
